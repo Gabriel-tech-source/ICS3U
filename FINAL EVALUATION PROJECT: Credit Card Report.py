5731cd010f5ab3d728f3874d7627d75973f37545
Gabriel <755505@pdsb.net>
2025-01-20 08:37:37 -0500

    Update FINAL EVALUATION PROJECT: Credit Card Report.py

--- FINAL EVALUATION PROJECT: Credit Card Report.py
+++ FINAL EVALUATION PROJECT: Credit Card Report.py
@@ -2,7 +2,7 @@
 # Author: Gabriel Abdulnour
 # Date: January 23 2025
 # Description: This program reads credit card data from the data.dat file, identifies expired or soon-to-expire cards,
-#              sorts the data by expiry date, and writes the results to an output file.
+#              sorts the data by expiry date, and writes the results to the output file of output.txt.
 
 def read_data(file_name):
     """
